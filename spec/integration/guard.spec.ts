26802008f25bf375158d373b4703e209bbad5bb5
Brandon Roberts <robertsbt.ags@gmail.com>
2016-04-20 15:47:02 -0500

    Stuff

---
+++ spec/integration/guard.spec.ts
@@ -0,0 +1,169 @@
+import {
+  describe,
+  beforeEach,
+  beforeEachProviders,
+  it,
+  iit,
+  TestComponentBuilder,
+  injectAsync,
+  expect
+} from 'angular2/testing';
+import { Component, provide } from 'angular2/core';
+import { Observable } from 'rxjs/Observable';
+import { LocationStrategy } from 'angular2/src/router/location/location_strategy';
+import { MockLocationStrategy } from 'angular2/src/mock/mock_location_strategy';
+import {
+  provideRouter,
+  createGuard,
+  Route
+} from '../../lib';
+
+@Component({
+  selector: 'guard-integration',
+  template: '',
+  directives: []
+})
+class TestComponent {}
+
+@Component({
+  selector: 'other',
+  template: 'Not Found',
+  directives: []
+})
+class OtherwiseComponent {}
+
+const authGuard = createGuard(function() {
+  return function() {
+    return Observable.of(false);
+  }
+}, [ ]);
+
+const compile = (tcb: TestComponentBuilder, template: string = '') => {
+  return tcb
+    .overrideTemplate(TestComponent, template)
+    .createAsync(TestComponent);
+};
+
+describe('Guard Integration', () => {
+  beforeEachProviders(() => [
+    provideRouter([
+      {
+        path: '/guarded',
+        guards: [ authGuard ],
+        component: TestComponent
+      },
+      {
+        path: '/**',
+        component: OtherwiseComponent
+      }
+    ]),
+    provide(LocationStrategy, { useClass: MockLocationStrategy })
+  ]);
+
+  it('should generate an href', injectAsync([TestComponentBuilder], (tcb) => {
+    return compile(tcb, '<a linkTo="/page">Page</a>')
+      .then((fixture) => {
+        fixture.detectChanges();
+        let compiled = fixture.debugElement.nativeElement;
+        let link: Element = compiled.querySelector('a');
+
+        expect(link.getAttribute('href')).toEqual('/page');
+      });
+  }));
+
+  it('should generate an href with a provided query params object', injectAsync([TestComponentBuilder], (tcb) => {
+    return compile(tcb, '<a linkTo="/page" [queryParams]="{id: 1}">Page</a>')
+      .then((fixture) => {
+        fixture.detectChanges();
+        let compiled = fixture.debugElement.nativeElement;
+        let link: Element = compiled.querySelector('a');
+
+        expect(link.getAttribute('href')).toEqual('/page?id=1');
+      });
+  }));
+
+  it('should generate an href with a provided query params string', injectAsync([TestComponentBuilder], (tcb) => {
+    return compile(tcb, '<a linkTo="/page" queryParams="id=1">Page</a>')
+      .then((fixture) => {
+        fixture.detectChanges();
+        let compiled = fixture.debugElement.nativeElement;
+        let link: Element = compiled.querySelector('a');
+
+        expect(link.getAttribute('href')).toEqual('/page?id=1');
+      });
+  }));
+
+  describe('When Clicked', () => {
+    it('should go to the provided URL', injectAsync([TestComponentBuilder, Router], (tcb, router) => {
+      let linkHref = '/page';
+      let queryParams = '{id: 1}';
+
+      return compile(tcb, '<a linkTo="' + linkHref + '" queryParams="' + queryParams + '">Page</a>')
+        .then((fixture) => {
+          fixture.detectChanges();
+          let compiled = fixture.debugElement.nativeElement;
+          let link = compiled.querySelector('a');
+
+          spyOn(router, 'go');
+
+          link.click();
+
+          expect(router.go).toHaveBeenCalledWith(linkHref, queryParams);
+        });
+    }));
+
+    it('should not prevent default behavior with a provided target', injectAsync([TestComponentBuilder, Router], (tcb, router) => {
+      let linkHref = '/page';
+      let queryParams = 'id=1';
+
+      return compile(tcb, '<a linkTo="' + linkHref + '" queryParams="' + queryParams + '" target="_blank">Page</a>')
+        .then((fixture) => {
+          fixture.detectChanges();
+          let compiled = fixture.debugElement.nativeElement;
+          let link = compiled.querySelector('a');
+
+          spyOn(router, 'go');
+
+          let instance = fixture.componentInstance.link;
+          let event = { button: 1 };
+          instance.onClick(event);
+
+          expect(router.go).not.toHaveBeenCalled();
+        });
+    }));
+
+    it('should not prevent default behavior with a combo click', injectAsync([TestComponentBuilder, Router], (tcb, router) => {
+      let linkHref = '/page';
+      let queryParams = 'id=1';
+
+      return compile(tcb, '<a linkTo="' + linkHref + '" queryParams="' + queryParams + '">Page</a>')
+        .then((fixture) => {
+          fixture.detectChanges();
+          let compiled = fixture.debugElement.nativeElement;
+          let link: Element = compiled.querySelector('a');
+          fixture.detectChanges();
+
+          let instance = fixture.componentInstance.link;
+
+          spyOn(router, 'go');
+
+          let events = [
+            { which: 1, ctrlKey: true },
+            { which: 1, metaKey: true },
+            { which: 1, shiftKey: true },
+            { which: 2 },
+            { button: 2 },
+            { ctrlKey: true },
+            { metaKey: true },
+            { shiftKey: true }
+          ];
+
+          events.forEach((event) => {
+            instance.onClick(event);
+          });
+
+          expect(router.go.calls.count()).toEqual(0);
+        });
+    }));
+  });
+});
